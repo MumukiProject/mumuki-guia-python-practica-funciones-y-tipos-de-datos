549e86ca7c6b87225cee6313d17189b7b724e5e3
Franco Bulgarelli <franco@mumuki.org>
2019-04-15 15:14:18 -0300

    Fixing test

--- 00013_¿Está cerca?/test.py
+++ 00013_¿Está cerca?/test.py
@@ -1,10 +1,26 @@
-def test_esta_cerca(443)(self): assert(esta_cerca(443))
-def test_esta_cerca(442)(self): assert(esta_cerca(442))
-def test_esta_cerca(441)(self): assert(esta_cerca(441))
-def test_esta_cerca(439)(self): assert(esta_cerca(439))
-def test_esta_cerca(438)(self): assert(esta_cerca(438))
-def test_esta_cerca(437)(self): assert(esta_cerca(437))
+def test_esta_cerca_443(self):
+  self.assertTrue(esta_cerca(443))
 
-def test_NO esta_cerca(440)(self): assert(!esta_cerca(440))
-def test_NO esta_cerca(420)(self): assert(!esta_cerca(420))
-def test_NO esta_cerca(460)(self): assert(!esta_cerca(460))
+def test_esta_cerca_442(self):
+  self.assertTrue(esta_cerca(442))
+
+def test_esta_cerca_441(self):
+  self.assertTrue(esta_cerca(441))
+
+def test_esta_cerca_439(self):
+  self.assertTrue(esta_cerca(439))
+
+def test_esta_cerca_438(self):
+  self.assertTrue(esta_cerca(438))
+
+def test_esta_cerca_437(self):
+  self.assertTrue(esta_cerca(437))
+
+def test_NO_esta_cerca_440(self):
+  self.assertTrue(not esta_cerca(440))
+
+def test_NO_esta_cerca_420(self):
+  self.assertTrue(not esta_cerca(420))
+
+def test_NO_esta_cerca_460(self):
+  self.assertTrue(not esta_cerca(460))
