bb587f27d82946285113306f160b3b08fb24deba
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-24 18:11:37 +0000

    Mumuki Export on 2019-04-24 18:11:37 +0000

--- 00007_Más Cartelitos/test.py
+++ 00007_Más Cartelitos/test.py
@@ -6,7 +6,7 @@
     self.assertEqual(escribir_cartelito("Dr.", "Julio", "Gelman", False), "Dr. Julio Gelman")
   
   def test_escribir_cartelito_Dra_Ana_Perez_true_es_Dra_Perez_(self):
-    self.assertEqual(escribir_cartelito("Dra.", "Ana", "Pérez", True), "Dra. Perez")
+    self.assertEqual(escribir_cartelito("Dra.", "Ana", "Perez", True), "Dra. Perez")
   
   def test_escribir_cartelito_Dr_Julio_Gelman_true_es_Dr_Gelman_(self):
     self.assertEqual(escribir_cartelito("Dr.", "Julio", "Gelman", True), "Dr. Gelman")
