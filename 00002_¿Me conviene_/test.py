92d9525b7c73b1241a3bcaa9bce4fda7e7793cea
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-24 17:26:40 +0000

    Mumuki Export on 2019-04-24 17:26:40 +0000

--- 00002_¿Me conviene_/test.py
+++ 00002_¿Me conviene_/test.py
@@ -1,16 +1,16 @@
   
   def test_me_conviene_25_8_es_falso(self):
-    self.assertEqual(not me_conviene(25, 8))
+    self.assertFalse(me_conviene(25, 8))
   
   def test_me_conviene_40_8_es_verdadero(self):
-    self.assertEqual(me_conviene(40, 8))
+    self.assertTrue(me_conviene(40, 8))
   
   def test_me_conviene_40_10_es_verdadero(self):
-    self.assertEqual(me_conviene(40, 10))
+    self.assertTrue(me_conviene(40, 10))
   
   def test_me_conviene_40_4_es_falso(self):
-    self.assertEqual(not me_conviene(40, 4))
+    self.assertFalse(me_conviene(40, 4))
   
   def test_me_conviene_50_16_es_falso(self):
-    self.assertEqual(not me_conviene(50, 16))
+    self.assertFalse(me_conviene(50, 16))
   
