b4f0355abd9fbd8d79bbc9f161feb870e194ea4d
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-25 14:51:56 +0000

    Mumuki Export on 2019-04-25 14:51:56 +0000

--- 00008_Cartelitos óptimos/test.py
+++ 00008_Cartelitos óptimos/test.py
@@ -3,7 +3,7 @@
     self.assertEqual(escribir_cartelito_optimo("Ing.", "Carla", "Toledo"), "Ing. Carla Toledo")
   
   def test_escribe_un_cartelito_largo_cuando_el_nombre_es_branco_luis(self):
-    self.assertEqual(escribir_cartelito_optimo("Ing.", "Branco", "Luis"), "Ing. Branco test_escribe_un_cartelito_largo_cuando_el_nombre_es_branco_luis")
+    self.assertEqual(escribir_cartelito_optimo("Ing.", "Branco", "Luis"), "Ing. Branco Luis")
   
   def test_escribe_un_cartelito_corto_cuando_el_nombre_es_estanislao_schwarzschild(self):
     self.assertEqual(escribir_cartelito_optimo("Dr.", "Estanislao", "Schwarzschild"), "Dr. Schwarzschild")
