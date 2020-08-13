428ff9fa52e838fc8738de185ccd87c73cc8b401
rocio@mumuki.org <rocio@mumuki.org>
2020-08-13 18:01:20 +0000

    Mumuki Export on 2020-08-13 18:01:20 +0000

--- 00011_¡Quiero retruco!/test.py
+++ 00011_¡Quiero retruco!/test.py
@@ -8,6 +8,6 @@
   def test_puntosDeEnvidoTotales_8_oro_2_bastos_es_8(self):
     self.assertEqual(puntos_de_envido_totales(5, "oro", 2, "bastos"), 5)
   
-  def test_puntosDeEnvidoTotales_6_copas_7_espadas_es_8(self):
+  def test_puntosDeEnvidoTotales_6_copas_7_espadas_es_7(self):
     self.assertEqual(puntos_de_envido_totales(6, "copas", 7, "espadas"), 7)
   
